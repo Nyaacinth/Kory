f7fd6daab562467169efd169aac41f7b045d0ed6
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2023-02-18 20:03:47 +0800

    Init

---
+++ app/MainApp.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "react"
+import { Text, View } from "react-native"
+
+export const MainApp: FunctionComponent = () => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
